41eba57ca70333c4eff3e092c8644ffc33a28383
juanparadeda <juanparadeda@gmail.com>
2022-04-18 17:25:27 -0300

    made cart.html responsive

--- js/accesoriesPage.js
+++ js/accesoriesPage.js
@@ -1,3 +1,5 @@
+// Este archivo solo interactúa con la página accesories.html . Esencialmente, del catálogo filtra los accesorios y los publica. Luego se define la función búsqueda para la barra de búsqueda
+
 const accesoriesCatalog = filterCategory(catalog, 'accesorio')
 cardHTMLGenerator(accesoriesCatalog);
 addProductPagesLinks();
